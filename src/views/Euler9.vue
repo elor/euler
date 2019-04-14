70136908b6b9522a9cb3111a995d6e6ea1813749
Erik E. Lorenz <erik@tuvero.de>
2019-04-14 10:25:58 +0200

    Solve 9

---
+++ src/views/Euler9.vue
@@ -0,0 +1,37 @@
+<template>
+  <problem
+    :input="input"
+    :challenge="challenge"
+    @input="value => (input = value)"
+  />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      input: 25
+    }
+  },
+  computed: {
+    challenge () {
+      const sum = this.input
+      for (let a = 1; a < sum / 3; a += 1) {
+        for (let b = a; b < sum - 2 * a; b += 1) {
+          const c = sum - a - b
+
+          if (c <= 0) {
+            console.log('I doofed up')
+            continue
+          }
+
+          if (a * a + b * b === c * c) {
+            return a * b * c
+          }
+        }
+      }
+      return 'nothing found'
+    }
+  }
+}
+</script>
