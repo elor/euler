7151e387687acb9464047203a29b0b4f08bb43f1
Erik E. Lorenz <erik@tuvero.de>
2019-04-14 10:30:16 +0200

    default to EulerTemplate

--- src/problems.js
+++ src/problems.js
@@ -1,5 +1,5 @@
 import { range } from 'lodash'
 
 export default range(1, 665).map(num =>
-  ({ component: () => import(`./views/Euler${num}`), path: `/${num}`, name: `Euler ${num}` })
+  ({ component: () => import(`./views/Euler${num}`).catch(() => import(`./views/EulerTemplate`)), path: `/${num}`, name: `Euler ${num}` })
 )
