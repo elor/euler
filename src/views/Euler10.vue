5b4862b614e0a01dd83b33aa99ebaea2652a4d1d
Erik E. Lorenz <erik@tuvero.de>
2019-04-14 10:28:13 +0200

    Solve 10

---
+++ src/views/Euler10.vue
@@ -0,0 +1,34 @@
+<template>
+  <problem
+    :input="input"
+    :min="3"
+    :challenge="challenge"
+    @input="value => (input = value)"
+  />
+</template>
+
+<script>
+function isPrime (number, primes) {
+  return !primes.some(prime => number % prime === 0)
+}
+
+export default {
+  data () {
+    return {
+      input: 10
+    }
+  },
+  computed: {
+    challenge () {
+      const primes = [2]
+      for (let number = 3; number < this.input; number += 2) {
+        if (isPrime(number, primes)) {
+          primes.push(number)
+        }
+      }
+
+      return primes.reduce((a, b) => a + b)
+    }
+  }
+}
+</script>
