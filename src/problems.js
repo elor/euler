10438b032942f0e94d921fcf1c41f1602890e65a
Erik E. Lorenz <erik@tuvero.de>
2019-04-14 08:57:09 +0200

    Solve 7

--- src/problems.js
+++ src/problems.js
@@ -4,6 +4,7 @@ import Euler0003 from './views/Euler0003'
 import Euler0004 from './views/Euler0004'
 import Euler0005 from './views/Euler0005'
 import Euler0006 from './views/Euler0006'
+import Euler0007 from './views/Euler0007'
 
 export default [
   { component: Euler0001, path: '/0001', name: 'Euler0001' },
@@ -11,5 +12,6 @@ export default [
   { component: Euler0003, path: '/0003', name: 'Euler0003' },
   { component: Euler0004, path: '/0004', name: 'Euler0004' },
   { component: Euler0005, path: '/0005', name: 'Euler0005' },
-  { component: Euler0006, path: '/0006', name: 'Euler0006' }
+  { component: Euler0006, path: '/0006', name: 'Euler0006' },
+  { component: Euler0007, path: '/0007', name: 'Euler0007' }
 ]
---
+++ src/views/Euler0007.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Euler 7</h1>
+    <label>input({{ input }}): </label>
+    <input v-model="input" type="number" min="1" />
+    <br />
+    <textarea :value="JSON.stringify(challenge)"></textarea>
+  </div>
+</template>
+
+<script>
+function isPrime (number, factors) {
+  return !factors.some(prime => number % prime === 0)
+}
+
+export default {
+  data () {
+    return {
+      input: 6
+    }
+  },
+  computed: {
+    challenge () {
+      const factors = [2]
+      for (let number = 3; factors.length < this.input; number += 2) {
+        if (isPrime(number, factors)) {
+          factors.push(number)
+        }
+      }
+
+      return factors.pop()
+    }
+  }
+}
+</script>
