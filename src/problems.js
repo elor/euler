84c22c7484d4f3bbf53eabfd32c935407ab35c34
Erik E. Lorenz <erik@tuvero.de>
2019-04-14 09:14:26 +0200

    Solve 8

--- src/problems.js
+++ src/problems.js
@@ -5,6 +5,7 @@ import Euler0004 from './views/Euler0004'
 import Euler0005 from './views/Euler0005'
 import Euler0006 from './views/Euler0006'
 import Euler0007 from './views/Euler0007'
+import Euler0008 from './views/Euler0008'
 
 export default [
   { component: Euler0001, path: '/0001', name: 'Euler0001' },
@@ -13,5 +14,6 @@ export default [
   { component: Euler0004, path: '/0004', name: 'Euler0004' },
   { component: Euler0005, path: '/0005', name: 'Euler0005' },
   { component: Euler0006, path: '/0006', name: 'Euler0006' },
-  { component: Euler0007, path: '/0007', name: 'Euler0007' }
+  { component: Euler0007, path: '/0007', name: 'Euler0007' },
+  { component: Euler0008, path: '/0008', name: 'Euler0008' }
 ]
